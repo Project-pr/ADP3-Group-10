c842b5d958e30135f1c72f3333ae082573674e92
Mxovela <yanela692@gmail.com>
2024-03-27 18:38:38 +0200

    first commit

--- src/main/java/za/co/cput/repository/EmployeeRepositoryImpl.java
+++ src/main/java/za/co/cput/repository/EmployeeRepositoryImpl.java
@@ -1,4 +1,20 @@
 package za.co.cput.repository;
 
+/* Author   : Mxolisi Ndovela
+ * Student no.: 217094740
+ * Subject  : Applications Development 3
+ * Date     :27 March 2024
+ * Assignment 1
+ */
+
+import za.co.cput.domain.Employee;
+
+import java.util.List;
+
 public interface EmployeeRepositoryImpl {
+    void create(Employee employee);
+    Employee read(int employeeId);
+    void update(Employee employee);
+    void delete(int employeeId);
+    List<Employee> getAll();
 }
